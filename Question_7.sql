5e2cfe518f6d603458405518c35c29a9643bea4d
Munisekhar05 <muniroyaj@gmail.com>
2025-08-13 07:47:00 +0530

    Delete Question_7.sql

--- Question_7.sql
+++
@@ -1,6 +0,0 @@
-#print a year and how many movies were released in that year starting with latest year
-
-   select release_year, count(*) as movies_count 
-   from movies 
-   group by release_year
-   order by release_year desc
