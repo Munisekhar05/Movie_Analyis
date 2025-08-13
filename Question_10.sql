f9e0bc7d6857a5bdbcdc86c4fe992d07de26a617
Munisekhar05 <muniroyaj@gmail.com>
2025-08-13 07:52:29 +0530

    Delete Question_10.sql

--- Question_10.sql
+++
@@ -1,7 +0,0 @@
-#Show all Telugu movie names (assuming you don't know language
-# id for Telugu)
-  
-   SELECT title	FROM movies m 
-   LEFT JOIN languages l 
-   ON m.language_id=l.language_id
-   WHERE l.name="Telugu"
