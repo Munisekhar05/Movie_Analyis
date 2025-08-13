37ed32d38567688a572dc25d201f1b6d1dc50646
Munisekhar05 <muniroyaj@gmail.com>
2025-08-13 07:50:26 +0530

    Delete Question_16.sql

--- Question_16.sql
+++
@@ -1,9 +0,0 @@
-#Rank movies by IMDb rating within each industry
-
-
-SELECT 
-    m.title,
-    m.industry,
-    m.imdb_rating,
-    Dense_rank() OVER (PARTITION BY m.industry ORDER BY m.imdb_rating DESC) AS rank_in_industry
-FROM movies m;
