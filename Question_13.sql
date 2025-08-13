ba92e26ca05d0542a3c2d096411181be555bbebb
Munisekhar05 <muniroyaj@gmail.com>
2025-08-13 07:51:41 +0530

    Delete Question_13.sql

--- Question_13.sql
+++
@@ -1,7 +0,0 @@
-# select all the movies with minimum and maximum release_year. Note that there 
-# can be more than one movies in min and max year hence output rows can be more than 2
-
-	select * from movies where release_year in (
-        (select min(release_year) from movies),
-		(select max(release_year) from movies)
-	)
