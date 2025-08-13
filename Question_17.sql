58c8d5cd7b7eef6ba359694a7cb04f61c6657370
Munisekhar05 <muniroyaj@gmail.com>
2025-08-13 07:50:08 +0530

    Delete Question_17.sql

--- Question_17.sql
+++
@@ -1,9 +0,0 @@
-#Find top-grossing movie(s) per studio
-
-SELECT 
-    m.studio,
-    m.title,
-    f.revenue,
-    DENSE_RANK() OVER (PARTITION BY m.studio ORDER BY f.revenue DESC) AS revenue_rank
-FROM movies m
-JOIN financials f ON m.movie_id = f.movie_id;
